b197c4ef9cc958aa18e36fc445614bd0ac5c70d0
advay2004 <140720821+AdvayKankaria@users.noreply.github.com>
2025-07-08 16:50:33 +0530

    Update server.js

--- secure_notes_app/backend/server.js
+++ secure_notes_app/backend/server.js
@@ -88,4 +88,5 @@ app.get("/notes", authMiddleware, async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
